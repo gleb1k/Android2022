a9b7d522ec4cf90c4249475db4199fa540785704
Gleb Gafeev <gleb.gafeev@mail.ru>
2022-10-01 23:28:57 +0300

    binding

--- app/src/main/java/com/example/android2022/MainActivity.kt
+++ app/src/main/java/com/example/android2022/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.android2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getSupportActionBar()!!.hide();
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also{
+            setContentView(it.root)
+        }
+        val binding = binding!!
     }
 }
