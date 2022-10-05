70dd3e3756bd6cf78b09e85c43556965cb125714
Gleb Gafeev <gleb.gafeev@mail.ru>
2022-10-05 23:34:59 +0300

    begin

---
+++ app/src/main/java/com/example/android2022/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.android2022
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android2022.databinding.FragmentSecondBinding
+
+class SecondFragment:Fragment(R.layout.fragment_second) {
+    private var binding: FragmentSecondBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSecondBinding.bind(view)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
