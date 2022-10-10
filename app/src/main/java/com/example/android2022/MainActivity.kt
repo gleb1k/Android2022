b93b47404c225cc670aa97f3c2f36abb040c8e1c
Гафеев Глеб <gleb.gafeev@mail.ru>
2022-10-10 15:28:42 +0300

    nothing

--- app/src/main/java/com/example/android2022/MainActivity.kt
+++ app/src/main/java/com/example/android2022/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.ActivityMainBinding.bind()
         setContentView(R.layout.activity_main)
 
     }
