16bd46efb791f249e00c533ac31efa4f36f07553
Gleb Gafeev <gleb.gafeev@mail.ru>
2022-11-29 07:22:11 +0300

    hw8

--- app/src/main/java/com/example/android2022/MainActivity.kt
+++ app/src/main/java/com/example/android2022/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.android2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    private var adapter: MusicAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        binding?.run {
+            adapter = MusicAdapter(
+                MusicRepository.musicList,
+                :: playMusic,
+                :: showMusicInfo
+                )
+
+
+        }
 
     }
+
+    private fun playMusic(music: Music) {
+
+    }
+
+    private fun showMusicInfo(music: Music) {
+
+    }
+
 }
---
+++ app/src/main/java/com/example/android2022/MusicRepository.kt
@@ -0,0 +1,7 @@
+package com.example.android2022
+
+object MusicRepository{
+    val musicList: List<Music> = arrayListOf(
+//        Music("REASON TO LIVE","DVRST","Phonk", R.)
+    )
+}
